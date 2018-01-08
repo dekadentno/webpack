1f71c84b4ef9b47c30286d42c2f13b29a1942ba7
dekadentno <matej.lazarevich@gmail.com>
2018-01-08 08:44:04 +0100

    Added ESLint with barrage config

--- template/src/App.vue
+++ template/src/App.vue
@@ -10,25 +10,25 @@
 </template>
 
 <script>
-{{#unless router}}
-import HelloWorld from './components/HelloWorld'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  {{#unless router}}
+  import HelloWorld from './components/HelloWorld'{{#if_eq lintConfig "none"}};{{/if_eq}}
 
-{{/unless}}
-export default {
-  name: 'app'{{#router}}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},
-  components: {
-    HelloWorld{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{/router}}
-}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    {{/unless}}
+  export default {
+    name: 'App'{{#router}}{{#if_eq lintConfig "none"}}{{/if_eq}}{{else}},
+        components: {
+          HelloWorld{{#if_eq lintConfig "none"}},{{/if_eq}}
+        }{{#if_eq lintConfig "none"}},{{/if_eq}}{{/router}}
+  }{{#if_eq lintConfig "none"}};{{/if_eq}}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- template/src/main.js
+++ template/src/main.js
@@ -2,26 +2,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
-import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Vue from 'vue'{{#if_eq lintConfig "none"}};{{/if_eq}}
+import App from './App'{{#if_eq lintConfig "none"}};{{/if_eq}}
 {{#router}}
-import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import router from './router'{{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/router}}
 {{#vueResource}}
-import VueResource from 'vue-resource'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import VueResource from 'vue-resource'{{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/vueResource}}
 {{#vueStash}}
-import VueStash from 'vue-stash'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import VueStash from 'vue-stash'{{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/vueStash}}
 {{#vuex}}
-import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Vuex from 'vuex'{{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/vuex}}
 {{#moment}}
-import moment from 'moment'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import moment from 'moment'{{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/moment}}
 {{#lodash}}
-import lodash from 'lodash'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-window._ = lodash{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import lodash from 'lodash'{{#if_eq lintConfig "none"}};{{/if_eq}}
+window._ = lodash{{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/lodash}}
 {{#socket}}
 /*
@@ -29,21 +29,21 @@ import VueSocketio from 'vue-socket.io';
 Vue.use(VueSocketio, 'http://socketserver.com:1923');
 */
 {{/socket}}
-  
+
 {{#vueResource}}
-Vue.use(VueResource){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.use(VueResource){{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/vueResource}}
 {{#vueStash}}
-Vue.use(VueStash){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.use(VueStash){{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/vueStash}}
 {{#vuex}}
-Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.use(Vuex){{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/vuex}}
   {{#moment}}
-Vue.use(moment){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.use(moment){{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/moment}}
-  
-Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+Vue.config.productionTip = false{{#if_eq lintConfig "none"}};{{/if_eq}}
 
 /* eslint-disable no-new */
 new Vue({
@@ -52,21 +52,12 @@ new Vue({
   router,
   {{/router}}
   {{#if_eq build "runtime"}}
-  render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+  render: h => h(App){{#if_eq lintConfig "none"}},{{/if_eq}}
   {{/if_eq}}
   {{#if_eq build "standalone"}}
   template: '<App/>',
-  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+  components: { App }
   {{/if_eq}}
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+}){{#if_eq lintConfig "none"}};{{/if_eq}}
 
-console.log("Init!");
-
-window._log = window.console.log.bind && window.console.log.bind(window.console) || function(){
-};
-window._wrn = window.console.warn.bind && window.console.warn.bind(window.console) || function(){
-};
-window._err = window.console.error.bind && window.console.error.bind(window.console) || function(){
-};
-window._info = window.console.info.bind && window.console.info.bind(window.console) || function(){
-};
+console.log('Init!');
