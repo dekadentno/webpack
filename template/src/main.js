e83e38abadf64deda0aa9fb1f27a011205b59b3b
dekadentno <matej.lazarevich@gmail.com>
2018-01-08 07:29:26 +0100

    Broke smth, fixed it

--- template/src/main.js
+++ template/src/main.js
@@ -7,7 +7,42 @@ import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{#router}}
 import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
-
+{{#vueResource}}
+import VueResource from 'vue-resource'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/vueResource}}
+{{#vueStash}}
+import VueStash from 'vue-stash'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/vueStash}}
+{{#vuex}}
+import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/vuex}}
+{{#moment}}
+import moment from 'moment'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/moment}}
+{{#lodash}}
+import lodash from 'lodash'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+window._ = lodash{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/lodash}}
+{{#socket}}
+/*
+import VueSocketio from 'vue-socket.io';
+Vue.use(VueSocketio, 'http://socketserver.com:1923');
+*/
+{{/socket}}
+  
+{{#vueResource}}
+Vue.use(VueResource){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/vueResource}}
+{{#vueStash}}
+Vue.use(VueStash){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/vueStash}}
+{{#vuex}}
+Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/vuex}}
+  {{#moment}}
+Vue.use(moment){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/moment}}
+  
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 /* eslint-disable no-new */
@@ -24,3 +59,14 @@ new Vue({
   components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   {{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+console.log("Init!");
+
+window._log = window.console.log.bind && window.console.log.bind(window.console) || function(){
+};
+window._wrn = window.console.warn.bind && window.console.warn.bind(window.console) || function(){
+};
+window._err = window.console.error.bind && window.console.error.bind(window.console) || function(){
+};
+window._info = window.console.info.bind && window.console.info.bind(window.console) || function(){
+};
