ea0a93b6684d90dd9eb5a456212202450956a41d
Matej Lazarević <matej.lazarevic@33barrage.com>
2018-01-08 09:02:52 +0100

    idk

--- template/src/main.js
+++ template/src/main.js
@@ -8,7 +8,6 @@ import App from './App'{{#if_eq lintConfig "none"}};{{/if_eq}}
 import router from './router'{{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/router}}
 {{#vueResource}}
-<<<<<<< HEAD
 import VueResource from 'vue-resource'{{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/vueResource}}
 {{#vueStash}}
@@ -23,22 +22,6 @@ import moment from 'moment'{{#if_eq lintConfig "none"}};{{/if_eq}}
 {{#lodash}}
 import lodash from 'lodash'{{#if_eq lintConfig "none"}};{{/if_eq}}
 window._ = lodash{{#if_eq lintConfig "none"}};{{/if_eq}}
-=======
-import VueResource from 'vue-resource'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/vueResource}}
-{{#vueStash}}
-import VueStash from 'vue-stash'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/vueStash}}
-{{#vuex}}
-import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/vuex}}
-{{#moment}}
-import moment from 'moment'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/moment}}
-{{#lodash}}
-import lodash from 'lodash'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-window._ = lodash{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
->>>>>>> master
 {{/lodash}}
 {{#socket}}
 /*
@@ -46,7 +29,6 @@ import VueSocketio from 'vue-socket.io';
 Vue.use(VueSocketio, 'http://socketserver.com:1923');
 */
 {{/socket}}
-<<<<<<< HEAD
 
 {{#vueResource}}
 Vue.use(VueResource){{#if_eq lintConfig "none"}};{{/if_eq}}
@@ -62,23 +44,6 @@ Vue.use(moment){{#if_eq lintConfig "none"}};{{/if_eq}}
 {{/moment}}
 
 Vue.config.productionTip = false{{#if_eq lintConfig "none"}};{{/if_eq}}
-=======
-  
-{{#vueResource}}
-Vue.use(VueResource){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/vueResource}}
-{{#vueStash}}
-Vue.use(VueStash){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/vueStash}}
-{{#vuex}}
-Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/vuex}}
-  {{#moment}}
-Vue.use(moment){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/moment}}
-  
-Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
->>>>>>> master
 
 /* eslint-disable no-new */
 new Vue({
@@ -93,10 +58,6 @@ new Vue({
   template: '<App/>',
   components: { App }
   {{/if_eq}}
-<<<<<<< HEAD
 }){{#if_eq lintConfig "none"}};{{/if_eq}}
-=======
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
->>>>>>> master
 
 console.log('Init!');
